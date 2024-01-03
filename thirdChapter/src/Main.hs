26e9cdbc580f40dfcf14980314c36d7493a3881c
toxypiks <laura.ganss@web.de>
2024-01-03 14:38:18 +0100

    Add solution for excercise 3.7 and 3.8 d

--- thirdChapter/src/Main.hs
+++ thirdChapter/src/Main.hs
@@ -1,6 +1,4 @@
-module Main (main) where
-
--- Excercise 3.7 Rewrite the definition of length non-recursively
+-- Excercise 3.7 Rewrite the definition of length non-recursively.
 
 my_length :: [Int] -> Int
 my_length xs = sum (map (const 1) xs)
@@ -8,5 +6,32 @@ my_length xs = sum (map (const 1) xs)
 list_length :: Int
 list_length = my_length[2,4,3,2,5,6]
 
-main = do
-  print $ list_length
+-- Implementation of fold function due to learning purposes
+
+my_fold :: (a -> b -> a) -> a -> [b] -> a
+my_fold func start_val [] = start_val
+my_fold func start_val (x:xs) = my_fold func (func start_val x) xs
+
+-- my_fold add 0 [1 ,3 , 4 ,6] = my_fold add (add 0 1) [3, 4 ,6]
+-- -> my_fold add 1 [3, 4, 6] = my_fold add (add 1 3) [4,6]
+-- -> my_fold add 4 [4, 6] = my_fold add (add 4 4) [6]
+-- -> my_fold add 8 [6] = my_fold add (add 8 + 6) []
+-- -> my_fold add 14 [] = 14
+
+my_add :: (Num a) => a -> a -> a
+my_add x y = x + y
+
+my_add_aux :: (Num a) => a -> b -> a
+my_add_aux x _ = x + 1
+
+list_length_two :: (Num a) => [a] -> Int
+list_length_two xs = my_fold my_add_aux 0 xs
+
+-- Excercise 3.8
+-- d) Add "pointwise" the element of a list of pairs.
+
+my_add_tuple :: (Int, Int) -> (Int, Int) -> (Int, Int)
+my_add_tuple (x, y) (a, b) = (x + a, y + b)
+
+my_add_tuple_list :: [(Int, Int)] -> (Int, Int)
+my_add_tuple_list xs = my_fold my_add_tuple (0, 0) xs
