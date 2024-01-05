8b183d7dd898c417faadb08c27e03a0cc3bcb9c4
toxypiks <laura.ganss@web.de>
2024-01-05 16:20:07 +0100

    Solve exercise 3.10 and 3.11

--- thirdChapter/src/Main.hs
+++ thirdChapter/src/Main.hs
@@ -1,3 +1,5 @@
+import Euterpea
+
 -- Excercise 3.7 Rewrite the definition of length non-recursively.
 
 my_length :: [Int] -> Int
@@ -55,3 +57,47 @@ my_add_tuple (x, y) (a, b) = (x + a, y + b)
 
 my_add_tuple_list :: [(Int, Int)] -> (Int, Int)
 my_add_tuple_list xs = my_fold my_add_tuple (0, 0) xs
+
+-- Excercise 3.9
+-- Define a function that combines a list of durations with a list of notes to create complete notes
+fuse :: [Dur] -> [Dur -> Music a] -> [Music a]
+fuse _ [] = []
+fuse [] _ = []
+fuse (d:dl) (n:nl) = (n d) : fuse dl nl
+
+-- Exercise 3.10
+
+max_abspitch :: [AbsPitch] -> AbsPitch
+max_abspitch [] = error "empty list"
+max_abspitch [x] = x
+max_abspitch (x1:x2:xs) = max_abspitch ((if x1 >= x2 then x1 else x2) :xs)
+
+min_abspitch :: [AbsPitch] -> AbsPitch
+min_abspitch [] = error "empty list"
+min_abspitch [x] = x
+min_abspitch (x1:x2:xs) = min_abspitch ((if x1 <= x2 then x1 else x2) :xs)
+
+
+my_max :: [Int] -> Int
+my_max [] = error "empty list"
+my_max [x] = x
+my_max (x1:x2:xs) | x1 >= x2 = my_max (x1:xs)
+                  | x1 < x2 = my_max (x2:xs)
+
+my_fold_max :: [Int] -> Int
+my_fold_max (x:xs) = foldr max (x) xs
+
+-- Exercise 3.11
+
+reverse_list :: [Int] -> [Int]
+reverse_list [] = []
+reverse_list (x:xs) = reverse_list xs ++ [x]
+
+
+make_list :: Int -> Int -> [Int]
+make_list x y | x < y = [x .. y]
+              | x > y = reverse_list [y .. x]
+              | x == y = [x]
+
+chrom :: Pitch -> Pitch -> [Music Pitch]
+chrom p1 p2 = map (note qn) $ map pitch $ make_list (absPitch p1) (absPitch p2)
