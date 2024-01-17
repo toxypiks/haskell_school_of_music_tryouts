48881e7e9e8cf4676eef75d872a1a7cb4eed7797
toxypiks <laura.ganss@web.de>
2024-01-17 15:49:32 +0100

    Rearrange parameter for uniformity

--- firstChapter/src/Main.hs
+++ firstChapter/src/Main.hs
@@ -4,12 +4,12 @@ import Euterpea
 
 -- Excercise 1.4: Modify the definitions of hNote and hList so that each take an extra argument that specifies the interval of harmonization
 
-hNote      :: Dur -> Pitch -> Int -> Music Pitch
-hNote d p h = note d p :=: note d (trans (-h) p)
+hNote      :: Int -> Dur -> Pitch -> Music Pitch
+hNote h d p = note d p :=: note d (trans (-h) p)
 
 hList           :: Int -> Dur -> [Pitch] -> Music Pitch
 hList h d []     = rest 0
-hList h d (p:ps) = hNote d p h :+: hList h d ps
+hList h d (p:ps) = hNote h d p :+: hList h d ps
 
 mel :: Int -> Music Pitch
 mel h = hList h qn [(F,4), (A,4), (F,4)]
