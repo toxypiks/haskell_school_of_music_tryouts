9ea774fe0e38951817677b0b335da8c0f0482d3c
toxypiks <laura.ganss@web.de>
2024-02-02 15:31:51 +0100

    Add function to solve exercise 5.2 WIP

--- fifthChapter/src/Main.hs
+++ fifthChapter/src/Main.hs
@@ -1,8 +1,15 @@
-import Euterpea
-
--- Define a function twice, given a function f, returns a function that applies f twice to its argument.
+-- Exercise 5.1 Define a function twice, given a function f, returns a function that applies f twice to its argument.
 
 twice    :: Num a => (a -> a) -> a -> a
 twice f y = (f (f y))
 
-my_res_sec = (twice (+1)) 2
+my_res = (twice (+1)) 2
+
+-- Exercise 5.2 Define a function power that takes a function f and an integer n and returns a function that applies the function f to its argument n times.
+
+power :: (Num a, Eq a) => (a -> a) -> a -> a -> a
+power f y 0 = y
+power f y n = let x = (f y)
+              in power f x (n-1)
+
+my_res_sec = power (+2) 5 1
