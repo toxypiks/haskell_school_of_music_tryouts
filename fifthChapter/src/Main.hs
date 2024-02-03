df5ab5cecea73bcdd22c5878263681ccf413c30a
toxypiks <laura.ganss@web.de>
2024-02-03 11:33:36 +0100

    Returning a function using where

--- fifthChapter/src/Main.hs
+++ fifthChapter/src/Main.hs
@@ -1,9 +1,21 @@
+-- 5.1 Sections
 -- Exercise 5.1 Define a function twice, given a function f, returns a function that applies f twice to its argument.
+
+-- First solution
 
 twice    :: Num a => (a -> a) -> a -> a
 twice f y = (f (f y))
 
 my_res = (twice (+1)) 2
+
+-- Actually returning a function using where
+
+twice_sec :: Num a => (a -> a) -> (a -> a)
+twice_sec f = twiceF
+          where twiceF a = f (f a)
+
+my_res_sec = (twice_sec (+1)) 2
+
 
 -- Exercise 5.2 Define a function power that takes a function f and an integer n and returns a function that applies the function f to its argument n times.
 
@@ -12,4 +24,4 @@ power f y 0 = y
 power f y n = let x = (f y)
               in power f x (n-1)
 
-my_res_sec = power (+2) 5 1
+my_res_pow = power (+2) 5 1
